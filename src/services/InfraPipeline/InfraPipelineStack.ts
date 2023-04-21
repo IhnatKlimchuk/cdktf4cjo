8cdf3a8d3bf77aceb8a1803aeb78cd44141e5ac2
Ihnat Klimchuk <Ihnat.Klimchuk@gmail.com>
2023-04-21 17:43:22 +0200

    Import resources

--- src/services/InfraPipeline/InfraPipelineStack.ts
+++ src/services/InfraPipeline/InfraPipelineStack.ts
@@ -1,21 +1,14 @@
 import { Construct } from "constructs";
 import { ServiceStack } from "../../common/ServiceStack";
 import { Environment } from "../../common/Environment";
-import { DataAzurermStorageAccount } from "@cdktf/provider-azurerm/lib/data-azurerm-storage-account";
 import { UserAssignedIdentity } from "@cdktf/provider-azurerm/lib/user-assigned-identity";
 import { RoleAssignment } from "@cdktf/provider-azurerm/lib/role-assignment";
-//import { StorageAccount } from "@cdktf/provider-azurerm/lib/storage-account";
-//import { StorageContainer } from "@cdktf/provider-azurerm/lib/storage-container";
+import { StorageAccount } from "@cdktf/provider-azurerm/lib/storage-account";
+import { StorageContainer } from "@cdktf/provider-azurerm/lib/storage-container";
 
 export class InfraPipelineStack extends ServiceStack {
     constructor(scope: Construct, env: Environment) {
         super(scope, "infra-pipeline", env);
-
-        // import existing account
-        new DataAzurermStorageAccount(this, "storage-account", {
-            resourceGroupName: this.resourceGroup.name,
-            name: "cdttf4cjostorage",
-        })
 
         const identity = new UserAssignedIdentity(this, "user-identity", {
             location: this.region,
@@ -24,22 +17,22 @@ export class InfraPipelineStack extends ServiceStack {
         })       
 
         new RoleAssignment(this, "role", {
-            principalId: identity.clientId,
+            principalId: identity.principalId,
             scope: `/subscriptions/${this.subscription}/resourceGroups/${this.resourceGroup.name}`,
             roleDefinitionName: "Storage Blob Data Owner",
         })
 
-        // const account = new StorageAccount(this, "storage-account", {
-        //     location: this.region,
-        //     accountTier: "Standard",
-        //     accountReplicationType: "LRS",
-        //     resourceGroupName: this.resourceGroup.name,
-        //     name: "cdttf4cjostorage",
-        // })
+        const account = new StorageAccount(this, "storage-account", {
+            location: this.region,
+            accountTier: "Standard",
+            accountReplicationType: "LRS",
+            resourceGroupName: this.resourceGroup.name,
+            name: "cdttf4cjostorage",
+        })
 
-        // new StorageContainer(this, "storage-container", {
-        //     name: "tfstate",
-        //     storageAccountName: account.name,
-        // })
+        new StorageContainer(this, "storage-container", {
+            name: "tfstate",
+            storageAccountName: account.name,
+        })
     }
 }
